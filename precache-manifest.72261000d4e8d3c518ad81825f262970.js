f4b2969182a4e9f184a29ab2902494d15e3c7d96
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-10 17:44:44 +0000

    Deploy mutoe/handnote to github.com/mutoe/handnote.git:gh-pages

---
+++ precache-manifest.72261000d4e8d3c518ad81825f262970.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c1d796d869233b33b8f6",
+    "url": "/js/chunk-vendors.3b67029f.js"
+  },
+  {
+    "revision": "62ebd6b8576b8b465ae4",
+    "url": "/js/app.c5e13f9f.js"
+  },
+  {
+    "revision": "4633a048ff5a74b83010e43309d6302b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "c1d796d869233b33b8f6",
+    "url": "/css/chunk-vendors.bc244610.css"
+  },
+  {
+    "revision": "62ebd6b8576b8b465ae4",
+    "url": "/css/app.2dfc6377.css"
+  }
+];
---
+++ precache-manifest.b59dfbaec8789faca8c39af15a577b4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1d796d869233b33b8f6",
+    "url": "/js/chunk-vendors-legacy.3b67029f.js"
+  },
+  {
+    "revision": "62ebd6b8576b8b465ae4",
+    "url": "/js/app-legacy.c5e13f9f.js"
+  },
+  {
+    "revision": "d69f212741bdf7510ca6a171fde9cfcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "c1d796d869233b33b8f6",
+    "url": "/css/chunk-vendors.bc244610.css"
+  },
+  {
+    "revision": "62ebd6b8576b8b465ae4",
+    "url": "/css/app.2dfc6377.css"
+  }
+];
