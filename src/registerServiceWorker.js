9a26251d4c2d42a27ea1245168ecfcee97bd1696
mutoe <mutoe@foxmail.com>
2018-11-13 17:43:58 +0800

    chore: 自动更新 service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,20 +1,26 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
+import { Message } from 'element-ui'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
+      console.log('Service worker is active.')
     },
-    cached () {
+    registered (registration) {
+      console.log('Service worker has been registered.')
+      registration.update()
+    },
+    cached (registration) {
       console.log('Content has been cached for offline use.')
     },
-    updated () {
+    updatefound (registration) {
+      console.log('New content is downloading.')
+    },
+    updated (registration) {
       console.log('New content is available; please refresh.')
+      Message.success('已获取新版本，将会在下次启动时应用')
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
